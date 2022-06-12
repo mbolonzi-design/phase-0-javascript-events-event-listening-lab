958c9fdbc0493f18ba57d60e02ba2fb3358a0613
mbolonzi-design <mbolonzi.design@gmail.com>
2022-06-12 13:31:53 +0300

    commit tree 34f4742c73f834c489f908f10ba59de5ea5f442e

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+// const input = document.getElementById('input');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// }); 
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    input.addEventListener('click', function(){
+        alert('I have been clicked');
+    });
 }
